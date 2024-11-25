3e93e89b1c753eab9960be8c93ce83d12b0054d6
k168ww <k168ww@gmail.com>
2024-11-25 23:26:50 +0900

    Fix: 중복 entity 삭제

--- demo/src/main/java/com/spring/demo/HostOCR/HostRepository.java
+++ demo/src/main/java/com/spring/demo/HostOCR/HostRepository.java
@@ -1,6 +1,6 @@
 package com.spring.demo.HostOCR;
 
-import com.spring.demo.entity.HostInfo;
+import com.spring.demo.security.model.HostInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
