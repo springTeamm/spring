18a349e40bb7ebcf63d062f09f170d89d9d13db0
k168ww <k168ww@gmail.com>
2024-12-02 16:23:29 +0900

    #fix: security config

--- demo/src/main/java/com/spring/demo/security/config/SecurityConfig.java
+++ demo/src/main/java/com/spring/demo/security/config/SecurityConfig.java
@@ -1,24 +1,13 @@
 package com.spring.demo.security.config;
 
-import com.spring.demo.security.repository.UserRepository;
-import com.spring.demo.security.security.SessionAuthentication;
-import com.spring.demo.security.service.CustomUserDetailsService;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
